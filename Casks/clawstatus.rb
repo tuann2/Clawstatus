3e77409b9272f6835021111f18f20edcd1ca28ab
tuan.nn <tuan.nn@amigo.vn>
2026-07-14 11:25:05 +0700

    Pass Homebrew Cask audit

--- Casks/clawstatus.rb
+++ Casks/clawstatus.rb
@@ -2,8 +2,7 @@ cask "clawstatus" do
   version "0.3.0"
   sha256 "1178af33c86087a4249208e247175e4924a1847bd97792d7757e9d6f869149a2"
 
-  url "https://github.com/tuann2/Clawstatus/releases/download/v#{version}/Clawstatus-#{version}-apple-silicon.dmg",
-      verified: "github.com/tuann2/Clawstatus/"
+  url "https://github.com/tuann2/Clawstatus/releases/download/v#{version}/Clawstatus-#{version}-apple-silicon.dmg"
   name "Clawstatus"
   desc "Menu bar monitor for remaining Claude Code usage"
   homepage "https://github.com/tuann2/Clawstatus"
